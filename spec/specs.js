fafbd78b9f42abcbe42c662b659c8e27165f6984
Ashlin Aronin & Samantha Maturen <student@epicodus.com>
2015-09-09 14:05:56 -0700

    Write all tests and make them fail

---
+++ spec/specs.js
@@ -0,0 +1,21 @@
+describe('isPalindrome', function() {
+    it("handles single letter words", function() {
+        expect(isPalindrome("a")).to.equal(true);
+    });
+
+    it("handles two letter words", function() {
+        expect(isPalindrome("if")).to.equal(false);
+    });
+
+    it("handles three letter words", function() {
+        expect(isPalindrome("mom")).to.equal(true);
+    });
+
+    it("handles celebrity palindromes", function() {
+        expect(isPalindrome("No, Mel Gibson is a casino's big lemon.")).to.equal(true);
+    });
+
+    it("handles Costner palindrome", function() {
+        expect(isPalindrome("Sis, ask Costner to not rent socks 'as is.'"));
+    });
+});
